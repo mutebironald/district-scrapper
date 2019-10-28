bc57af8be2f7e6b29724f60c04a0aa59e6eab2e8
Ronald Mutebi <ronald.mutebi@andela.com>
2019-10-28 19:00:58 +0300

    add tests for package

--- test/test.js
+++ test/test.js
@@ -1,25 +1,45 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+/* eslint-disable no-undef */
+var chai = require("chai");
+const expect = chai.expect;
+
+const { path } = require("../api/index.js");
+
+const sinon = require('sinon');
+
+
+const obj = {
+  fn: () => {}
+}
+
+
+describe ("Should fetch url data", function() {
+
+  it("UrlData is called", done => {
+    console.log(path, 'path')
+    const resolvingPromise = new Promise((resolve, reject) => {
+      resolve(path);
+      reject("Failed to retrieve data")
     });
-  });
-});
-
-beforeEach(function(){
-  brow
-})
-
-describe('index', function(){
-  describe('#UrlData', function(){
-    it('should fetch data from wikipedia', function(done){
-      var url = "https://en.wikipedia.org/wiki/Districts_of_Uganda";
-      return new Promise(function (resolve){
-
+    resolvingPromise
+      .then(result => {
+        expect(result).to.equal(new Array(135));
+        // ex
       })
+      .catch(error => console.log(error.message))
+      .finally(done);
+  });
 
+  // it('handles a promise rejection', () => {
+  //   new Promise(reject => {
+  //     setTimeout(() => reject('woops'), 500)
+  //   })
+  // })
 
-    })
+  it('stubs a func', function(){
+    sinon.stub(obj, 'fn').returns(Promise.resolve())
   })
-})
+
+
+
+
+});
