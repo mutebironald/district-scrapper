348bbcb666636438529904076dddec19db54e07c
Ronald Mutebi <ronald.mutebi@andela.com>
2019-11-04 19:16:58 +0300

    fix vulnerables

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const axios = require("axios");
 const cherio = require("cherio");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const url =
+  "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/wiki/Districts_of_Uganda";
 
-const url = "https://en.wikipedia.org/wiki/Districts_of_Uganda";
+let data = [];
 
 let getData = html => {
   //these are the districts that were difficult to track in the DOM.
@@ -15,7 +20,7 @@ let getData = html => {
     "Obongi"
   ];
   //we make a new array of districts. Here we spread out the untracked districts and append those gotten through dom manipulation
-  let data = [...untracked_districts];
+  data = [...untracked_districts];
   //cherio is for dom manipulation
   const $ = cherio.load(html);
   $("table.wikitable tr td a").each((i, elem) => {
@@ -44,13 +49,20 @@ let getData = html => {
   });
 };
 
-const urlData = () => {
-  return axios
-    .get(url)
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const urlData = async () => {
+  await axios
+    .get(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      json: true
+    })
     .then(response => getData(response.data))
     .catch(error => console.log(error));
+  return data;
 };
 
 module.exports = {
-  path: urlData()
+  getDistricts: urlData()
 };
